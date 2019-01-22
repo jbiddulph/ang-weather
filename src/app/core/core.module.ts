ba6eea5fb0d3c279b179ac08545270bd0238a61c
John Biddulph <john.mbiddulph@gmail.com>
2019-01-22 14:53:57 +0000

    first commit

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
+import { MaterialModule } from '../shared/material.module';
+import { ToolbarComponent } from '../ui/toolbar/toolbar.component';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [ToolbarComponent],
+  exports: [ToolbarComponent],
+  imports: [
+    CommonModule,
+    SharedModule,
+    MaterialModule,
+    RouterModule
+  ]
+})
+export class CoreModule { }
---
+++ src/app/shared/interfaces/weather.ts
@@ -0,0 +1,9 @@
+export interface Weather {
+    city: string;
+    country: string;
+    image: string;
+    description: string;
+    temerature: number;
+    lat?: number;
+    long?: number;
+}
