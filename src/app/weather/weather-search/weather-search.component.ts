3a6d139ca89d7d2b6f35799b825b39cf8637fe77
John Biddulph <john.mbiddulph@gmail.com>
2019-01-27 18:05:23 +0000

    JSON output

--- src/app/weather/weather-search/weather-search.component.ts
+++ src/app/weather/weather-search/weather-search.component.ts
@@ -14,6 +14,7 @@ export class WeatherSearchComponent implements OnInit {
   constructor(private weatherService: WeatherService) { }
 
   ngOnInit() {
+    console.log(this.weather);
   }
 
   search() {
